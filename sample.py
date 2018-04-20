64124ec0be5ca86896141f2b29773ffcc9892ae6
Taiki Horiuchi <h.tik9603052@gmail.com>
2018-04-20 14:45:01 +0900

    read_csv
    Signed-off-by: Taiki Horiuchi <h.tik9603052@gmail.com>

--- sample.py
+++ sample.py
@@ -1,10 +1,6 @@
-import numpy
-import math
-from matplotlib import pyplot
 import csv
 
-csv_file = open("./TEST_STOCK.csv", "r", encoding="ms932", errors="", newline="" )
-#リスト形式
-A = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
-#辞書形式
-B = csv.DictReader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
+with open("./input_data/lea_sig.csv", "r", encoding="utf-8") as f_csv_input:
+    o_csv = csv.reader(f_csv_input)
+    l_data=[list(map(float,row)) for row in o_csv]
+    
